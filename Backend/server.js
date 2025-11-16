1b8ce672e6a60560a2efeecf233073d484ad7640
John Alex S. Taguiam <taguiamjohnalex@gmail.com>
2025-11-16 19:57:03 +0800

    Added routes for different request , installed mongoose and bycrypt

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,28 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+app.get("/api/dashboard", (req, res) => {
+  res.status(200).send("Dashboard Loaded succesfully");
+});
+
+app.get("/api/customers", (req, res) => {
+  res.status(200).send("Customers loaded successfully");
+});
+
+app.get("/api/customers/:id", (req, res) => {
+  res.status(200).send("customers  details");
+});
+
+app.post("/api/add", (req, res) => {
+  res.status(201).send("New customer added succesfully");
+});
+
+app.put("/api/customers/:id", (req, res) => {
+  res.status(200).send("edit customer added succesfully");
+});
+
+app.delete("/api/customers/:id", (req, res) => {
+  res.status(200).send("customer deleted succesfully");
 });
 
 app.listen(PORT, () => {
