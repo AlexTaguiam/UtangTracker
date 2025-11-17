1391b47f6f45bd1919f898c9876e813f71fef4d8
John Alex S. Taguiam <taguiamjohnalex@gmail.com>
2025-11-17 13:34:46 +0800

    Connected to mongoDB using connection string from env file with dotenv, Additionally i created the model for the customer.

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://taguiamjohnalex_db_user:2tzy2Zqadt7k4XkY@cluster0.jslczr7.mongodb.net/?appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MONGODB CONNECTED SUCCESSFULLY");
   } catch (error) {
     console.error("Error Connecting to MongoDB", error);
+    process.exit(1);
   }
 };
---
+++ Backend/src/models/customer.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const utangSchema = new mongoose.Schema({
+  product: { type: String, required: true },
+  amount: { type: Number, required: true },
+  price: { type: Number, required: true },
+  total: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+  status: { typeof: String, enum: ["unpaid", "paid"], default: "unpaid" },
+});
+
+const customerSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  history: [utangSchema],
+});
+
+export default mongoose.model("Customer", customerSchema);
