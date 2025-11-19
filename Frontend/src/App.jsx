5e109b94cca0512726b931e33b9db1a1843f17a7
John Alex S. Taguiam <taguiamjohnalex@gmail.com>
2025-11-19 15:23:44 +0800

    installed tailwindCss

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,16 +5,19 @@ import CustomersPage from "./pages/CustomersPage";
 import CustomerDetailePage from "./pages/CustomerDetailePage";
 import AddUtangPage from "./pages/AddUtangPage";
 import AllUtangPage from "./pages/AllUtangPage";
+import toast from "react-hot-toast";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/dashboard" element={<DashBoardPage />} />
-      <Route path="/customers" element={<CustomersPage />} />
-      <Route path="/customers/:id" element={<CustomerDetailePage />} />
-      <Route path="/addCustomer" element={<AddUtangPage />} />
-      <Route path="/allUtang" element={<AllUtangPage />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/dashboard" element={<DashBoardPage />} />
+        <Route path="/customers" element={<CustomersPage />} />
+        <Route path="/customers/:id" element={<CustomerDetailePage />} />
+        <Route path="/addCustomer" element={<AddUtangPage />} />
+        <Route path="/allUtang" element={<AllUtangPage />} />
+      </Routes>
+    </div>
   );
 }
 
